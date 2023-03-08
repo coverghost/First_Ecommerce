c514b6dedf6e11a589a6c6fd1ee03ddc9fd7ff44
Ayush Arya <ayush@brightdigitalgold.com>
2023-03-08 23:14:05 +0530

    navbar

---
+++ src/components/homepage.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from "react";
+import "./Styles/style.css"
+
+const EommNavbar = () => {
+
+    return (
+        <>
+            <header>
+                <navbar>
+                    <ul className="navbar-list">
+                        <a href="#"> HOME </a>
+                        <a href="#"> ABOUT US </a>
+                        <a href="#"> LOGIN </a>
+                        <a href="#"> CONTACT US </a>
+                    </ul>
+                </navbar>
+            </header>
+
+        </>
+    )
+}
+
+export default EommNavbar;
